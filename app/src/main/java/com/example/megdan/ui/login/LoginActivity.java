649bbf909dd2eec7384780eab5310e978b0f779b
meggles711 <mhuber@gatech.edu>
2020-01-22 01:35:52 -0500

    Integrated login with landing page. Added Toasts for incorrect log in information.

--- app/src/main/java/com/example/megdan/ui/login/LoginActivity.java
+++ app/src/main/java/com/example/megdan/ui/login/LoginActivity.java
@@ -1,60 +1,61 @@
 package com.example.megdan.ui.login;
 
 
+import android.content.Context;
+import android.content.Intent;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.widget.EditText;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.megdan.R;
+import com.example.megdan.ui.other.Landing;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-
-import static com.example.megdan.R.id.login;
-import static com.example.megdan.R.id.register;
 //import com.google.firebase.quickstart.auth.R
 
 public class LoginActivity extends AppCompatActivity {
 
 
     private FirebaseAuth mAuth;
-    public FirebaseUser user;
+    //public FirebaseUser user;
     private EditText email;
     private EditText password;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        email = findViewById(R.id.email);
+        email = findViewById(R.id.username);
         password = findViewById(R.id.password);
 
         mAuth = FirebaseAuth.getInstance();
 
-        View.OnClickListener listener = new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                int i = v.getId();
-                if (i == R.id.register) {
-                    createAccount(email.getText().toString(), password.getText().toString());
-                } else if (i == R.id.login) {
-                    signIn(email.getText().toString(), password.getText().toString());
-                }
-            }
-        };
+        final Button registerButton = findViewById(R.id.register);
+        registerButton.setOnClickListener(new View.OnClickListener() {
 
-        findViewById(R.id.login).setOnClickListener(listener);
-        findViewById(R.id.register).setOnClickListener(listener);
+            public void onClick(View v) {
+                createAccount(email.getText().toString(), password.getText().toString());
+            }
+        });
+
+        final Button loginButton = findViewById(R.id.login);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                signIn(email.getText().toString(), password.getText().toString());
+            }
+        });
+
     }
 
     private void createAccount(String email, String password) {
@@ -66,11 +67,16 @@ public class LoginActivity extends AppCompatActivity {
                         if (task.isSuccessful()) {
                             // Sign in success, update UI with the signed-in user's information
                             Log.d("login", "createUserWithEmail:success");
-                            user = mAuth.getCurrentUser();
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Intent intent = new Intent(LoginActivity.this, Landing.class);
+                            startActivity(intent);
                         } else {
                             // If sign in fails, display a message to the user.
-                            Log.w("login", "createUserWithEmail:failure", task.getException());
-                            user = null;
+                            Log.d("login", "createUserWithEmail:failure", task.getException());
+                            Context context = getApplicationContext();
+                            CharSequence text = "User could not be created. Make password longer or use a different email";
+                            int duration = Toast.LENGTH_SHORT;
+                            Toast.makeText(context, text, duration).show();
                         }
                     }
                 });
@@ -85,11 +91,16 @@ public class LoginActivity extends AppCompatActivity {
                         if (task.isSuccessful()) {
                             // Sign in success, update UI with the signed-in user's information
                             Log.d("login", "signInWithEmail:success");
-                            user = mAuth.getCurrentUser();
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Intent intent = new Intent(LoginActivity.this, Landing.class);
+                            startActivity(intent);
                         } else {
                             // If sign in fails, display a message to the user.
-                            Log.w("login", "signInWithEmail:failure", task.getException());
-                            user = null;
+                            Log.d("login", "signInWithEmail:failure", task.getException());
+                            Context context = getApplicationContext();
+                            CharSequence text = "User could not be logged on. Email or password incorrect.";
+                            int duration = Toast.LENGTH_SHORT;
+                            Toast.makeText(context, text, duration).show();
                         }
                     }
                 });
@@ -97,7 +108,7 @@ public class LoginActivity extends AppCompatActivity {
 
     private void signOut(){
         mAuth.signOut();
-        user = null;
+        //user = null;
     }
 
 
